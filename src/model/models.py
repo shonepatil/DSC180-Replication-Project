4ddce85bb5d38c6c942cd2c249f76cea1a853d6a
shonepatil <s3patil@ucsd.edu>
2021-10-25 16:07:01 -0700

    Fixed GCN to allow hidden layer config

--- src/model/models.py
+++ src/model/models.py
@@ -6,11 +6,11 @@ sys.path.insert(0, 'src/model')
 from layers import GraphConvolution
 
 class GCN(nn.Module):
-    def __init__(self, nfeat, nclass, dropout):
+    def __init__(self, nfeat, nhid, nclass, dropout):
         super(GCN, self).__init__()
 
-        self.gc1 = GraphConvolution(nfeat, nfeat)
-        self.gc2 = GraphConvolution(nfeat, nclass)
+        self.gc1 = GraphConvolution(nfeat, nhid)
+        self.gc2 = GraphConvolution(nhid, nclass)
         self.dropout = dropout
 
     def forward(self, x, adj):
