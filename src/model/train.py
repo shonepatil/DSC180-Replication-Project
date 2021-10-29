ef12f50c2a2d1550cfa480856a6bc450a901e512
shonepatil <s3patil@ucsd.edu>
2021-10-28 21:13:38 -0700

    Added hidden layers to GCN approach to allow for learning

--- src/model/train.py
+++ src/model/train.py
@@ -55,7 +55,7 @@ def train_test(A, X, y, idx_train, idx_val, idx_test,
         model.train()
         optimizer.zero_grad()
         output = model(X, A)
-        loss_train = F.nll_loss(output[idx_train].cuda(), y[idx_train].cuda())
+        loss_train = F.binary_cross_entropy(output[idx_train].cuda(), y[idx_train].cuda())
         acc_train = accuracy(output[idx_train], y[idx_train])
         loss_train.backward()
         optimizer.step()
@@ -65,20 +65,19 @@ def train_test(A, X, y, idx_train, idx_val, idx_test,
         model.eval()
         output = model(X, A)
 
-        loss_val = F.nll_loss(output[idx_val].cuda(), y[idx_val].cuda())
+        loss_val = F.binary_cross_entropy(output[idx_val].cuda(), y[idx_val].cuda())
         acc_val = accuracy(output[idx_val], y[idx_val])
         print('Epoch: {:04d}'.format(epoch+1),
             'loss_train: {:.4f}'.format(loss_train.item()),
             'acc_train: {:.4f}'.format(acc_train.item()),
             'loss_val: {:.4f}'.format(loss_val.item()),
             'acc_val: {:.4f}'.format(acc_val.item()),
-            'time: {:.4f}s'.format(time.time() - t),
-            'weights: {}'.format(str(list(model.parameters())[0].grad)))
+            'time: {:.4f}s'.format(time.time() - t))
 
     def test():
         model.eval()
         output = model(X, A)
-        loss_test = F.nll_loss(output[idx_test].cuda(), y[idx_test].cuda())
+        loss_test = F.binary_cross_entropy(output[idx_test].cuda(), y[idx_test].cuda())
         acc_test = accuracy(output[idx_test], y[idx_test])
         print("Test set results:",
             "loss= {:.4f}".format(loss_test.item()),
