c16c12e9fce69258521e1a1eaad88065e327ac4e
shonepatil <s3patil@ucsd.edu>
2021-11-01 20:48:09 -0700

    CORA analysis

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -5,6 +5,7 @@ import torch
 import numpy as np
 import os
 from sklearn.preprocessing import StandardScaler
+from node2vec import Node2Vec
 
 import sys
 sys.path.insert(0, 'src/model')
@@ -15,8 +16,10 @@ def load_data(path, dataset, train, val, test, include_ad_hoc_feat=False):
     print('Loading {} dataset...'.format(dataset))
     
     # Load data
-    nodes = pd.read_csv(os.path.join(os.path.dirname(__file__), '{}{}target.csv'.format(path, dataset)), sep=',')
-    edges = pd.read_csv(os.path.join(os.path.dirname(__file__), '{}{}edges.csv'.format(path, dataset)), sep=',')
+    # nodes = pd.read_csv(os.path.join(os.path.dirname(__file__), '{}{}target.csv'.format(path, dataset)), sep=',')
+    # edges = pd.read_csv(os.path.join(os.path.dirname(__file__), '{}{}edges.csv'.format(path, dataset)), sep=',')
+    nodes = pd.read_csv(os.path.join(os.path.dirname(__file__), '{}{}.content'.format(path, dataset)), sep='\t', header=None),
+    edges = pd.read_csv(os.path.join(os.path.dirname(__file__), '{}{}.cites'.format(path, dataset)), sep='\t', header=None)
     
     # Construct graph
     G = nx.Graph(name = 'G')
@@ -34,8 +37,10 @@ def load_data(path, dataset, train, val, test, include_ad_hoc_feat=False):
     
     #Get the Adjacency Matrix (A) and Node Features Matrix (X) as numpy array
     A = torch.FloatTensor(nx.adjacency_matrix(G).todense())
-    X = nodes.iloc[:, [1, 3, 4]].to_numpy().astype(float)
-    y = encode_onehot(nodes.iloc[:, 2].to_numpy())
+    # X = nodes.iloc[:, [1, 3, 4]].to_numpy().astype(float)
+    # y = encode_onehot(nodes.iloc[:, 2].to_numpy())
+    X = nodes.drop([0, 1434], axis=1).to_numpy().astype(float)
+    y = encode_onehot(nodes[1434].to_numpy())
 
     # Include Ad-Hoc graph variables
     if include_ad_hoc_feat:
@@ -46,6 +51,15 @@ def load_data(path, dataset, train, val, test, include_ad_hoc_feat=False):
     idx_train = range(train)
     idx_val = range(train, train+val)
     idx_test = range(train+val, train+val+test)
+
+    # node2vec setup
+    # TODO:  REMOVE THIS
+    if False:
+        node2vec = Node2Vec(G, dimensions=64, walk_length=30, num_walks=200, workers=4)
+        # Embed nodes
+        mod = node2vec.fit(window=10, min_count=1, batch_words=4)
+        emb = np.array(mod.wv.vectors)
+        X = np.c_[X, emb].astype(float)
 
     I = torch.eye(A.shape[0]) #create Identity Matrix of A
     A_hat = A + I #add self-loop to A
